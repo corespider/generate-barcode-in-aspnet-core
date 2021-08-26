192a3191d7b9bc2500a14f2005b5bce2e483c953
Your Name <53593343+CoreProgramm@users.noreply.github.com>
2021-08-27 00:22:05 +0530

    Add project files.

---
+++ CP.BarCode/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using CP.BarCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP.BarCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult Index(string barcode)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //The Image is drawn based on length of Barcode text.
+                using (Bitmap bitMap = new Bitmap(barcode.Length * 40, 80))
+                {
+                    //The Graphics library object is generated for the Image.
+                    using (Graphics graphics = Graphics.FromImage(bitMap))
+                    {
+                        //The installed Barcode font.
+                        Font oFont = new Font("IDAutomationHC39M Free Version", 16);
+                        PointF point = new PointF(2f, 2f);
+
+                        //White Brush is used to fill the Image with white color.
+                        SolidBrush whiteBrush = new SolidBrush(Color.White);
+                        graphics.FillRectangle(whiteBrush, 0, 0, bitMap.Width, bitMap.Height);
+
+                        //Black Brush is used to draw the Barcode over the Image.
+                        SolidBrush blackBrush = new SolidBrush(Color.Black);
+                        graphics.DrawString("*" + barcode + "*", oFont, blackBrush, point);
+                    }
+
+                    //The Bitmap is saved to Memory Stream.
+                    bitMap.Save(ms, ImageFormat.Png);
+
+                    //The Image is finally converted to Base64 string.
+                    ViewBag.BarcodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
